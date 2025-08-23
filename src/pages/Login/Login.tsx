83198b8c063e9ed07a184dd816f8da2868ed91eb
Famaso13 <karlo.vlah13@gmail.com>
2025-08-23 19:40:55 +0200

    Added support for switching between signin and login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,11 +1,18 @@
 import "./login.css";
 import logo from "../../assets/logoWhite.png";
+import { useState } from "react";
 
 const Login = () => {
+    const [isLogin, setIsLogin] = useState(false);
+
     return (
         <div className="full-screen">
             <div className="half">
-                <img className="full-screen" src="/login.png" alt="driver" />
+                <img
+                    className="full-screen"
+                    src={isLogin ? "/login.png" : "/signin.png"}
+                    alt={isLogin ? "driverLogIn" : "driverSignIn"}
+                />
             </div>
             <div className="half">
                 <div className="space">

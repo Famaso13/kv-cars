872dd766165815da5051ae76f187b66fb2015641
Famaso13 <karlo.vlah13@gmail.com>
2025-08-27 05:39:41 +0200

    Added leaderboards for tracks

---
+++ src/components/Listing/Listing.tsx
@@ -0,0 +1,43 @@
+import type { Listings } from "../../types/listing";
+import "./listing.scss";
+
+interface ListingProps {
+    position?: number | string;
+    listing: Listings;
+    tempUnit?: string;
+    title?: boolean;
+}
+
+const Listing: React.FC<ListingProps> = ({ position, listing, tempUnit, title }) => {
+    return (
+        <div className={title ? "title listing" : "listing"}>
+            {position === 1 ? (
+                <div className="medal">
+                    <p className="position-first">{position}</p>
+                </div>
+            ) : position === 2 ? (
+                <div className="medal">
+                    <p className="position-second">{position}</p>
+                </div>
+            ) : position === 3 ? (
+                <div className="medal">
+                    <p className="position-third">{position}</p>
+                </div>
+            ) : (
+                <p>{position}</p>
+            )}
+            <p>{listing.username}</p>
+            <p>{listing.car}</p>
+            <p>{listing.category}</p>
+            <p>{listing.tyre}</p>
+            <p>{listing.weather}</p>
+            <p>
+                {listing.trackTemp}
+                {!title && tempUnit}
+            </p>
+            <p>{listing.date}</p>
+        </div>
+    );
+};
+
+export default Listing;
